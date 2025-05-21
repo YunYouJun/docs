e4b87ce981170b225d2447a44a9699ff987c4f56
YunYouJun <me@yunyoujun.cn>
2025-05-21 21:05:00 +0800

    chore: upgrade deps & add cli to install deps

---
+++ packages/docs/bin/index.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+'use strict'
+
+import { run } from '../dist/index.mjs'
+
+run()
---
+++ packages/docs/cli/index.ts
@@ -0,0 +1,49 @@
+import process, { title } from 'node:process'
+import prompts from 'prompts'
+
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import { version } from '../package.json'
+
+const cli = yargs(hideBin(process.argv))
+  .scriptName('docs')
+  .usage('$0 [args]')
+  .version(version)
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .command(
+    'deps',
+    '安装所需的依赖',
+    args =>
+      args
+        .options('yes', {
+          alias: 'y',
+          type: 'boolean',
+          description: 'Default to yes, do not prompt',
+          default: false,
+        }),
+    async (argv) => {
+      if (!argv.y) {
+        const res = await prompts([
+          {
+            type: 'multiselect',
+            name: 'deps',
+            message: '选择要安装的依赖',
+            choices: [
+              { title: '@shikijs/vitepress-twoslash', value: '@shikijs/vitepress-twoslash' },
+              { title: 'unocss', value: 'unocss' },
+              { title: 'unplugin-vue-components', value: 'unplugin-vue-components' },
+              { title: 'vite-plugin-vue-devtools', value: 'vite-plugin-vue-devtools' },
+            ],
+          },
+        ])
+        console.log(res)
+      }
+    },
+  )
+  .help()
+
+export function run() {
+  cli.parse()
+}
